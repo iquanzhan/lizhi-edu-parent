1dcca36f8f6ab1b59c5ce5b699dd1bc6bfb79bc7
Cheng xiaoxiao <iquanzhan@163.com>
2020-03-29 20:43:03 +0800

    初始化项目

---
+++ lizhi-edu-user/src/main/java/com/chengxiaoxiao/edu/UserApplication.java
@@ -0,0 +1,16 @@
+package com.chengxiaoxiao.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Cheng XiaoXiao  (🍊 ^_^ ^_^)
+ * @Date: 2020/3/29 8:26 下午
+ * @Description:
+ */
+@SpringBootApplication
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+}
---
+++ lizhi-edu-user/src/main/java/com/chengxiaoxiao/edu/config/DruidConfiguration.java
@@ -0,0 +1,45 @@
+package com.chengxiaoxiao.edu.config;
+
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Druid配置类
+ *
+ * @Author: Cheng XiaoXiao  (🍊 ^_^ ^_^)
+ * @Date: 2020/3/29 8:40 下午
+ * @Description:
+ */
+@Configuration
+@Slf4j
+public class DruidConfiguration {
+
+    @Bean
+    public ServletRegistrationBean druidServlet() {
+        log.info("init Druid Servlet Configuration ");
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
+        // IP白名单
+//        servletRegistrationBean.addInitParameter("allow", "*");
+        // IP黑名单(共同存在时，deny优先于allow)
+//        servletRegistrationBean.addInitParameter("deny", "192.168.1.100");
+        //控制台管理用户
+        servletRegistrationBean.addInitParameter("loginUsername", "admin");
+        servletRegistrationBean.addInitParameter("loginPassword", "admin");
+        //是否能够重置数据 禁用HTML页面上的“Reset All”功能
+        servletRegistrationBean.addInitParameter("resetEnable", "false");
+        return servletRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new WebStatFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        return filterRegistrationBean;
+    }
+}
